12a165e13dbf34a3f3920fb45d7889c2638e94f7
Tomato Ketchup <r@554.jp>
2016-06-06 13:07:13 +0900

    Fix cgo sample

--- slide/cgo.go
+++ slide/cgo.go
@@ -3,10 +3,11 @@ package main
 // #include <stdio.h>
 // #include <stdlib.h>
 import "C"
+
 import "unsafe"
 
 func main() {
-	cstr := C.CString("Hello, cgo !\n")
-	C.printf(cstr)
+	cstr := C.CString("Hello, cgo !")
+	C.puts(cstr)
 	C.free(unsafe.Pointer(cstr))
 }
